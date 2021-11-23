1d82b40a2e898e721ed94e597c0dc0fba20579e5
unknown <hetketan@gmail.com>
2021-11-23 17:25:24 -0400

    First Commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    Main m1 = new Main();
+    @Test
+    public void mainTest()
+    {
+        assertNotNull(m1);
+    }
+
+}
