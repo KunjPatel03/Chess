3505bfee1b6f0e28be2efbede4e67fc85054379a
Het Ketanbhai Shah <ht699147@dal.ca>
2021-12-04 17:46:48 -0400

    Initial display message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String args[]){
-        System.out.println("Welcome to the Chess!");
+    public static void main(String args[]) {
+        System.out.println("Hello and welcome to the Chess!");
+        System.out.println("");
+        System.out.println("Press 1 to play a new game");
     }
 }
