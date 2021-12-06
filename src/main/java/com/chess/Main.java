206b5d54f86f5210e3e2396e49b29c0255523a22
Het Ketanbhai Shah <ht699147@dal.ca>
2021-12-06 18:20:47 -0400

    Initializing Moves Module

--- src/main/java/com/chess/Main.java
+++ src/main/java/com/chess/Main.java
@@ -3,5 +3,6 @@ package com.chess;
 public class Main {
     public static void main(String args[]){
         System.out.println("Welcome to the Chess!");
+        System.out.println("Move Module");
     }
 }
